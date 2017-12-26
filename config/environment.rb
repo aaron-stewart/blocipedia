2273f58579ef7b3c6d2d2798bb01ab24d16be1b3
Aaron Stewart <aaron.stewart.contact@gmail.com>
2017-12-26 11:34:54 -0600

    Reconfigure sendgrid api for better security.

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require_relative 'application'
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   :user_name => 'apikey',
-  :password => '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',
+  :password => secret_key_sendgrid,
   :domain => 'blocipedia.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
